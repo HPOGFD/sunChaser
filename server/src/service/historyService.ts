236305c4b99b83a6899e4de5e034aa70cac75de9
Harry Oyarvide <poyarvide87@yahoo.com>
2024-11-27 11:32:37 -0500

    Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -25,7 +25,17 @@ class HistoryService {
     });
   }
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
-  // async getCities() {}
+  async getCities() {
+    return await this.read().then((cities) => {
+      let parsedCities: City[] = [];
+      try {
+        parsedCities = JSON.parse(cities);
+      } catch (error) {
+        console.error(`Error parsing cities: ${error}`);
+      }
+      return parsedCities;
+    });
+  }
   // TODO Define an addCity method that adds a city to the searchHistory.json file
   // async addCity(city: string) {}
   // * BONUS TODO: Define a removeCity method that removes a city from the searchHistory.json file

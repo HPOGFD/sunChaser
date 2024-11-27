46041104341aecd6bb175ad3383f8198da9cbcce
Harry Oyarvide <poyarvide87@yahoo.com>
2024-11-27 15:01:10 -0500

    POST request with city name to retrieve data. Getting data using weatherservice. save city to HistoryService

--- server/src/routes/api/weatherRoutes.ts
+++ server/src/routes/api/weatherRoutes.ts
@@ -6,14 +6,24 @@ import HistoryService from '../../service/historyService.js';
 import WeatherService from '../../service/weatherService.js';
 
 // TODO: POST Request with city name to retrieve weather data
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   const { city } = req.body;
    if (!city){
     res.status(400).json({message: 'City name is required'});
    }
-  // TODO: GET weather data from city name
-  // TODO: save city to search history
+   try {
+    // Get weather data using WeatherService
+    const weatherData = await WeatherService.getWeatherForCity(city);
+
+    // Save city to search history using HistoryService
+    await HistoryService.addCity(city);
+
+    res.status(200).json({ weather: weatherData, message: 'City saved to history' });
+  } catch (error) {
+    res.status(500).json({ message: 'Failed to retrieve weather data', error: error.message });
+  }
 });
+
 
 // TODO: GET search history
 router.get('/history', async (req, res) => {});

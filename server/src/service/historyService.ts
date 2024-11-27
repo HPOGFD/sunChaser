82765db832fba06db5d8145389ad95f79bcaaa4e
Harry Oyarvide <poyarvide87@yahoo.com>
2024-11-27 11:24:31 -0500

    Define a read method that reads from the searchHistory.json file. created this file inside my db folder

--- server/src/service/historyService.ts
+++ server/src/service/historyService.ts
@@ -12,7 +12,12 @@ class City {
 // TODO: Complete the HistoryService class
 class HistoryService {
   // TODO: Define a read method that reads from the searchHistory.json file
-  // private async read() {}
+  private async read() {
+    return await fs.readFile('searchHistory.json', {
+      encoding: 'utf-8',
+      flag : 'a+'
+    });
+  }
   // TODO: Define a write method that writes the updated cities array to the searchHistory.json file
   // private async write(cities: City[]) {}
   // TODO: Define a getCities method that reads the cities from the searchHistory.json file and returns them as an array of City objects
